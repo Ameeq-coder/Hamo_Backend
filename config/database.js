19e43f96d1d2292e1db5cddbd20e13e21d7de263
Ameeq-coder <ahmadameeq19@gmail.com>
2025-05-20 03:00:58 +0500

    Fix Sequelize config for Vercel

--- config/database.js
+++ config/database.js
@@ -1,12 +1,17 @@
-const {Sequelize}= require("sequelize")
+const { Sequelize } = require("sequelize");
 
+const env = process.env.NODE_ENV || 'development';
+const config = require('./config')[env];
 
-const env=process.env.NODE_ENV || 'development'
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  {
+    host: config.host,
+    port: config.port,
+    dialect: config.dialect
+  }
+);
 
-const config=require('./config');
-
-
-const sequelize= new Sequelize(config[env]);
-
-
-module.exports=sequelize;
+module.exports = sequelize;
